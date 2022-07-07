2e427b6899e484c266d3bd37b06f1a6e3f62c2b3
Joe Kagumba <63820705+JRKagumba@users.noreply.github.com>
2022-07-07 13:15:09 -0400

    updated description

--- Python/sudoku_solver.py
+++ Python/sudoku_solver.py
@@ -1,3 +1,10 @@
+"""
+Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
+
+The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
+"""
+
+
 def sudoku(puzzle):
     
     letters =['A','B','C','D','E','F','G','H','I']
